e2f7eefdcb9560b8b5bdd4ceba554dde67508f87
Soutaro Matsumoto <matsumoto@soutaro.com>
2016-05-18 22:57:33 +0900

    Fix error

--- analyzer.h
+++ analyzer.h
@@ -6,9 +6,4 @@
 class NullCheckAction : public clang::ASTFrontendAction {
 public:
   virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance &Compiler, clang::StringRef InFile);
-  virtual bool BeginSourceFileAction(clang::CompilerInstance &CI, clang::StringRef Filename);
-  virtual void EndSourceFileAction();
-
-private:
-  std::vector<ErrorMessage> *Errors;
 };
